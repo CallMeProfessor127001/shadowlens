311a543e58e1368a108d3427d37d106174d2b307
ProfessorVishal <107393840+CallMeProfessor127001@users.noreply.github.com>
2024-02-15 12:52:34 +0000

    Create DEBIANrequirements.sh

---
+++ DEBIANrequirements.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
+sudo apt-get clean
+
+sudo apt-get install figlet
+sudo apt-get install cowsay
+sudo apt-get install lolcat
+sudo apt-get install toilet
