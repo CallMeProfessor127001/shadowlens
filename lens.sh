dfc614d3dc8f80ab4c4a7e549f615230fc300762
ProfessorVishal <107393840+CallMeProfessor127001@users.noreply.github.com>
2024-02-21 19:05:33 -0500

    Update lens.sh

--- lens.sh
+++ lens.sh
@@ -15,7 +15,7 @@ printf "\n"
 dependencies() {
 
 
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
+command -v php > /dev/null 2>&1 || { echo >&2 "Shadowlens require php but it's not installed. Install it. Aborting."; exit 1; }
  
 
 
